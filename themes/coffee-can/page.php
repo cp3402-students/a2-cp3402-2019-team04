c9027720855e87af8539052b3fed43b633569320
Harmon Singh <harmon.singh@my.jcu.edu.au>
2019-05-24 05:16:45 +1000

    Some basic formatting to pages

--- themes/coffee-can/page.php
+++ themes/coffee-can/page.php
@@ -12,22 +12,15 @@
  * @package CoffeeCan
  */
 
-get_header();
-?>
+get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
 
 		endwhile; // End of the loop.
 		?>
