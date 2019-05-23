fb0faf32d0f5ed0d6fdbe9904f71f0167f0807d1
hamsingh <harmon.singh@my.jcu.edu.au>
2019-05-24 04:20:38 +1000

    Fix header issues

--- themes/coffee-can/header.php
+++ themes/coffee-can/header.php
@@ -34,9 +34,8 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php the_custom_logo(); ?>
-			<div class="site-branding__text"
+			<div class="site-branding__text">
 			<?php
-			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
